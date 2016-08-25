7761caaaa4deae0ec414387dbd0130bbd600f22e
Munyo Frey <munyofrey@gmail.com>
2016-08-25 16:32:42 -0700

    Updated pokemon list to include image and type of pokemon

--- frontend/components/pokemon/pokemon_index_item.jsx
+++ frontend/components/pokemon/pokemon_index_item.jsx
@@ -6,9 +6,18 @@ class PokemonIndexItem extends React.Component{
     super(props);
   }
 
+  _handleClick() {
+    console.log("i clicked :)",this.props.router);
+    this.props.router.push(`pokemon/${this.props.pokemon.id}`);
+  }
+
   render() {
-    debugger;
-    return( <li>{this.props.pokemon.name}</li>)
+
+    return( <li className="pokemon-index-item" onClick={this._handleClick.bind(this)}>
+        <span>{this.props.pokemon.name}</span>
+        <img src={this.props.pokemon.image_url}/>
+        <span>{this.props.pokemon.poke_type}</span>
+    </li>);
   }
 
 }
--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -4,12 +4,13 @@ import PokemonIndexContainer from './pokemon/pokemon_index_container';
 import { Provider } from 'react-redux';
 import { Router, Route, hashHistory } from 'react-router';
 import * as ACTIONS from '../actions/pokemon_actions';
+import PokemonIndexItem from './pokemon/pokemon_index_item';
 
 const Root = ({store}) => (
   <Provider store={store}>
     <Router history={hashHistory}>
       <Route path='/' component={PokemonIndexContainer} onEnter={store.dispatch(ACTIONS.requestAllPokemon())} >
-        
+        <Route path="pokemon/:id" component={PokemonIndexItem}/>
       </Route>
     </Router>
   </Provider>
